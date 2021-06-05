609a542a8cfd2ebe3661769e1a6fe58e8d14140d
jmc-smoothstack <84944129+jmc-smoothstack@users.noreply.github.com>
2021-06-05 10:33:53 -0400

    Update library.java

--- library.java
+++ library.java
@@ -6,26 +6,29 @@ import java.io.*;
 
 public class library{
 	String name, address;
+	//books[][] is the catalog of books in the libraries system.
 	String[][] books = {{"Lost Tribe by Sidney Sheldon", "1"}, {"The Haunting by Stepehen King", "2"}, {"Microtrends by Mark Penn", "3"}};
-
+	//Vector of bookLoanEntry objects to track a library's loans 
 	Vector <bookLoanEntry> bookLoans = new Vector<bookLoanEntry>();
-
-
+	//Contructors 
 	library(){}
 	library(String name, String address){
 		this.name = name; 
 		this.address = address; 
 	}
+	//createLoan() creates a new loan entry and adds it to Vector of loans 
 	public void createLoan(int userCardNumber, String bookTitle){
 
 		bookLoanEntry loan = new bookLoanEntry(userCardNumber, bookTitle);
 		bookLoans.add(loan);
+		//bookOut() subtracts 1 book from avaliable books
 		bookOut(bookTitle);
 		System.out.println("Loan successfully created");
 
 	}
+	//removes loan entry from Vector and adds copy back to inventory 
 	public void returnBook(int cardNumber, String bt){
-		int position = 0; 
+		int position = 0; //tracks poistion in Vector 
 		for(bookLoanEntry loan : bookLoans)
 		{
 			if(loan.userCardNumber == cardNumber)
@@ -37,27 +40,34 @@ public class library{
 			}
 			position +=1;
 		}
+		//removes bookLoanEntry() by position in Vector()
 		bookLoans.remove(position);
+
+		//adds copy back to inventory / raises availble copies for the specific book by 1 
+		for(String[] book : books){
+			if(book[0]==bt){
+				book[1] = Integer.toString(Integer.parseInt(book[1])+1);
+			}
+		}
 		System.out.println("remove succesful");
 	}
 
+	//addBooks() adds copies of a book in books[][] 
 	public void addBooks(String bookTitle, int booksToAdd){
-
 		for(String[] book : books)
 		{
-			if(book[0]==bookTitle){
+			if(book[0]==bookTitle){ 
 				book[1] = Integer.toString(Integer.parseInt(book[1])+booksToAdd);
 				System.out.println("copies successfully updated - copies: " + book[1]);
 			}
 		}
-
-
 	}
-
+	//get method to return Vector of book loans 
 	public Vector<bookLoanEntry> getBookLoans(){
 		return bookLoans;
 	}
 
+	//bookOut is used to subtract 1 from avalible books in the ibraries inventory  
 	public void bookOut(String bookTitle){
 		for(int i=0; i<books.length;i++){
 			//subtract book from inventory 
@@ -67,18 +77,21 @@ public class library{
 			}
 		}
 	}
+	//print method to show all books (titles and copies) in book catalog 
 	public int showBooks(){
 		int c = 0; 
 		for(int i=0; i<books.length; i++){
 			System.out.println(i+1+") "+books[i][0] + "  copies: " + books[i][1]);
 			c = i; 
 		}
+		//reutrns index for quit line 
 		System.out.println(c+2+") Quit to previous");
 		return c+2; 
 	}
 
+	//print method shows active loans 
 	public void showLoans(){
-
+		//checks if there is a loan in the loan array 
 		if(bookLoans.size() > 0)
 		{
 			int c = 0; 
@@ -91,48 +104,14 @@ public class library{
 		}else{System.out.println("No loans in system");}
 
 	}
-
+	//set method to set / change  library name 
 	public void setName(String s){
 		name = s; 
 		System.out.println("name successfully set");
 	}
-
+	//set method to set / change library address 
 	public void setAddress(String s){
 		address = s;
 		System.out.println("address successfully set");
-	}
-
-	public void libraryOptions(Scanner sc){
-		System.out.println("1) Update library details \n2) Add copies of Book to Branch \n3) Quit to previous");
-		int input = sc.nextInt();
-		switch(input){
-			case 1:
-			System.out.println("1) Update name \n2) Update address");
-			int input2 = sc.nextInt();
-			switch(input2)
-			{
-				case 1:
-				//changeName(sc);
-				case 2:
-				//changeAddress(sc);
-			}
-			case 2:   //add copies of books to branch 
-			for(int i=0; i<books.length; i++){
-				System.out.println(i+1+") "+books[i][0]);
-			}
-			int input3 = sc.nextInt();
-			System.out.println(books[input3-1][0] + " - existing number of copies: " + books[input3-1][1]);
-			System.out.println("Enter new number of copies");
-			int input4 = sc.nextInt();
-			books[input3-1][1] = Integer.toString(input4);
-			System.out.println("__copies updated__("+books[input3-1][1]+")");
-			case 3:
-			break;
-		}
-
-	}
-	public static void main (String args[]){
-		library lib = new library();
-		//lib.createLoan(12212, "jdfkgjfdl;k");
 	}
 }
